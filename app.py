839680e5f627670b3ea2f800a1287bdaa9baa40f
plavy <tinplavec@gmail.com>
2024-04-10 15:42:15 +0200

    topic 9, db and file storage

--- app.py
+++ app.py
@@ -1,35 +1,64 @@
 from threading import Lock
 from flask import Flask, render_template, session, request, jsonify, url_for
 from flask_socketio import SocketIO, emit, disconnect    
-import time
-import random
+import MySQLdb       
 import math
+import time
+import configparser as ConfigParser
+import random
 
 async_mode = None
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'secret!'
+## drop table poit.graph;
+## create table poit.graph ( id varchar(10), hodnoty varchar(16000) );
+
+config = ConfigParser.ConfigParser()
+config.read('config.cfg')
+myhost = config.get('mysqlDB', 'host')
+myuser = config.get('mysqlDB', 'user')
+mypasswd = config.get('mysqlDB', 'passwd')
+mydb = config.get('mysqlDB', 'db')
+
+dataFile = './data_file'
+
+app.config['SECRET_KEY'] = 'not_for_production_use!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock() 
 
+run = None
 
 def background_thread(args):
     count = 0
     dataList = []
+    db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)          
     while True:
+        socketio.sleep(1)
+        if not run:
+          if len(dataList)>0:
+            print('Data list:')
+            print(str(dataList))
+            fuj = str(dataList).replace("'", "\"")
+            cursor = db.cursor()
+            cursor.execute("SELECT MAX(id) FROM graph")
+            maxid = cursor.fetchone()[0]
+            if maxid is None:
+              maxid = 0
+            cursor.execute("INSERT INTO graph (id, hodnoty) VALUES (%s, %s)", (int(maxid) + 1, fuj))
+            db.commit()
+            print(f"Data list stored to the database with index {int(maxid) + 1}.")
+            with open(dataFile, "a+") as f:
+            	f.write(f"{fuj}\n")
+            	print(f"Data list stored to the file.")
+            dataList = []
+          continue
         if args:
           A = dict(args).get('A')
-          print(f"A is set to {A}")
+          print(f"A: {A}, run: {run}")
         else:
-          A = 1 
-        #print A
-        #print args
-        socketio.sleep(1)
-        if not dict(args).get("run"):
-          print("Message emitting is currently stopped.")
-          continue
+          A = 1
         count += 1
         t = time.time()
         x = count*0.1
@@ -42,17 +71,33 @@ def background_thread(args):
           "y2": y2}
         dataList.append(dataDict)
         if len(dataList)>0:
-          print('Data list:')
-          print(str(dataList))
-          print(str(dataList).replace("'", "\""))
-        socketio.emit('my_response',
+          socketio.emit('my_response',
                       {'data': y1, 'count': count, 'x': x, 'y1': y1, 'y2': y2},
                       namespace='/test')
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
-  
+
+@app.route('/graph', methods=['GET', 'POST'])
+def graph():
+    return render_template('graph.html', async_mode=socketio.async_mode)
+
+@app.route('/db/<string:num>')
+def dbdata(num):
+  db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)
+  cursor = db.cursor()
+  print(num)
+  cursor.execute("SELECT hodnoty FROM  graph WHERE id=%s", num)
+  rv = cursor.fetchone()
+  return str(rv[0])
+
+@app.route('/file/<string:num>')
+def readmyfile(num):
+    fo = open(dataFile, "r")
+    rows = fo.readlines()
+    return rows[int(num)-1]
+
 @socketio.on('my_event', namespace='/test')
 def test_message(message):   
     session['receive_count'] = session.get('receive_count', 0) + 1 
@@ -61,16 +106,17 @@ def test_message(message):
          {'data': message['value'], 'count': session['receive_count']})
  
 @socketio.on('start', namespace='/test')
-def test_start():   
-    print("RECEIVED START SIGNAL")
-    session['run'] = True
+def test_start():
+    global run
+    run = True
     emit('my_response',
          {'data': 'Start', 'count': session['receive_count']})
-  
+
 @socketio.on('stop', namespace='/test')
-def test_stop():   
-    print("RECEIVED STOP SIGNAL")
-    session['run'] = False
+def test_stop():
+    global run
+    run = False
+    print("Message emitting is currently stopped.")
     emit('my_response',
          {'data': 'Stop', 'count': session['receive_count']})
  
